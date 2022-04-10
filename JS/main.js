cd24c65115b25381ff5fad2b714a1353c384e949
Davinci9T <igonevv@gmail.com>
2022-04-10 22:51:19 +0500

    v2.0

--- JS/main.js
+++ JS/main.js
@@ -1,69 +1,6 @@
-// МЕНЮ
-
 window.addEventListener('DOMContentLoaded', function () {
-  // const burger = document.querySelector('#burger');
-  // const nav = document.querySelector('#nav');
 
-  // document.body.addEventListener("click", function (evt) {
-  //   if (evt.target.closest('#burger')) {
-  //     burger.classList.add('is-active');
-  //     nav.classList.add('is-active');
-  //   }
-  //   else if (!evt.target.closest('.header__nav')) {
-  //     burger.classList.remove('is-active');
-  //     nav.classList.remove('is-active');
-  //   }
-  // })
-
-  document.querySelector('#burger').addEventListener('click', function () {
-    document.querySelector('#burger').classList.toggle('is-active');
-    document.querySelector('#nav').classList.toggle('is-active');
-    document.querySelector('body').classList.toggle('lock');
-  })
-
-  document.querySelector('main').addEventListener('click', function () {
-    document.querySelector('#burger').classList.remove('is-active');
-    document.querySelector('#nav').classList.remove('is-active');
-    document.querySelector('body').classList.remove('lock');
-  })
-
-  document.querySelector('.header__nav-search').addEventListener('click', function () {
-    document.querySelector('#burger').classList.remove('is-active');
-    document.querySelector('#nav').classList.remove('is-active');
-    document.querySelector('body').classList.remove('lock');
-  })
-
-  // ПОИСК
-
-  document.querySelector('#search').addEventListener('click', function () {
-    document.querySelector('#search').classList.add('search-active');
-    document.querySelector('.nav-search-form').classList.add('search-active');
-    document.querySelector('.search-input').classList.add('search-active');
-    document.querySelector('#search-close').classList.add('search-active')
-  })
-
-  document.querySelector('#search-close').addEventListener('click', function () {
-    document.querySelector('#search').classList.remove('search-active');
-    document.querySelector('.nav-search-form').classList.remove('search-active');
-    document.querySelector('.search-input').classList.remove('search-active');
-    document.querySelector('#search-close').classList.remove('search-active');
-  })
-
-  document.querySelector('#burger').addEventListener('click', function () {
-    document.querySelector('#search').classList.remove('search-active');
-    document.querySelector('.nav-search-form').classList.remove('search-active');
-    document.querySelector('.search-input').classList.remove('search-active');
-    document.querySelector('#search-close').classList.remove('search-active');
-  })
-
-  document.querySelector('.header__nav-link').addEventListener('click', function () {
-    document.querySelector('#search').classList.remove('search-active');
-    document.querySelector('.nav-search-form').classList.remove('search-active');
-    document.querySelector('.search-input').classList.remove('search-active');
-    document.querySelector('#search-close').classList.remove('search-active');
-  })
-
-  // СЛАЙДЕР
+  // hero-СЛАЙДЕР ======================================================================================================================================================
 
   const swiper = new Swiper('.swiper', {
     slidesPerView: 1,
@@ -75,38 +12,95 @@ window.addEventListener('DOMContentLoaded', function () {
       delay: 3500,
       disableOnInteraction: false,
     },
+    mouseTouch: true,
+  })
+
+  // МЕНЮ ======================================================================================================================================================
+
+  document.querySelector('#burger').addEventListener('click', function () {
+    document.querySelector('#burger').classList.toggle('is-active');
+    document.querySelector('#nav').classList.toggle('is-active');
+  })
+
+  //при клике на бургер-меню закрывает открытый поиск
+  document.querySelector('#burger').addEventListener('click', function () {
+    document.querySelector('#search').classList.remove('search-active');
+    document.querySelector('.nav-search-form').classList.remove('search-active');
+    document.querySelector('.search-input').classList.remove('search-active');
+    document.querySelector('#search-close').classList.remove('search-active');
+  })
+
+  // ПОИСК ======================================================================================================================================================
+
+  //лупа открывает поиск
+  document.querySelector('#search').addEventListener('click', function () {
+    document.querySelector('#search').classList.add('search-active');
+    document.querySelector('.nav-search-form').classList.add('search-active');
+    document.querySelector('.search-input').classList.add('search-active');
+    document.querySelector('#search-close').classList.add('search-active')
+  })
+
+  //крестик закрывает поиск
+  document.querySelector('#search-close').addEventListener('click', function () {
+    document.querySelector('#search').classList.remove('search-active');
+    document.querySelector('.nav-search-form').classList.remove('search-active');
+    document.querySelector('.search-input').classList.remove('search-active');
+    document.querySelector('#search-close').classList.remove('search-active');
+  })
+
+  //при клике на поиск заврывается открытый бургер-меню
+  document.querySelector('.header__nav-search').addEventListener('click', function () {
+    document.querySelector('#burger').classList.remove('is-active');
+    document.querySelector('#nav').classList.remove('is-active');
+  })
+
+})
+
+// ТАБЫ ======================================================================================================================================================
+
+//активный таб
+function slidesPlugin(activeSlide = 0) {
+  const slides = document.querySelectorAll('.working__day-link');
+
+  slides[activeSlide].classList.add('active');
+
+  for (const slide of slides) {
+    slide.addEventListener('click', () => {
+      clearActiveClasses();
+      slide.classList.add('active');
+    });
+  }
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove('active');
+    });
+  }
+}
+
+slidesPlugin();
+
+document.querySelectorAll('.tabs__btn').forEach(function (tabsBtn) {
+  tabsBtn.addEventListener('click', function (event) {
+    const path = event.currentTarget.dataset.path
+
+    document.querySelectorAll('.tab-content').forEach(function (tabContent) {
+      tabContent.classList.remove('tab-content-active')
+    });
+    document.querySelector(`[data-target="${path}"]`).classList.add('tab-content-active');
   })
 })
 
-// ТАБЫ
+// faq-АККОРДЕОН
 
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelectorAll('.tabs__btn').forEach(function (tabsBtn) {
-    tabsBtn.addEventListener('click', function (event) {
-      const path = event.currentTarget.dataset.path
-
-      document.querySelectorAll('.tab-content').forEach(function (tabContent) {
-        tabContent.classList.remove('tab-content-active')
-      });
-      document.querySelector(`[data-target="${path}"]`).classList.add('tab-content-active');
-    })
-  })
-})
-
-// АККОРДЕОН
-
-document.addEventListener('DOMContentLoaded', function () {
-  $(".js-accordion").accordion({
-    collapsible: true,
-    active: false,
-    icons: false,
-    heightStyle: 'content'
-  });
-
-
+$(".js-accordion").accordion({
+  collapsible: true,
+  active: false,
+  icons: false,
+  heightStyle: 'content'
 });
 
-// КНОПКА НАВЕРХ
+// КНОПКА НАВЕРХ ======================================================================================================================================================
 
 jQuery(document).ready(function () {
   var btn = $('#button');
