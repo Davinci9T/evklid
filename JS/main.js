db478e634738f431e39e1038ae8d110fc0dc4871
Davinci9T <igonevv@gmail.com>
2022-04-10 23:58:11 +0500

    //

--- JS/main.js
+++ JS/main.js
@@ -62,65 +62,65 @@ window.addEventListener('DOMContentLoaded', function () {
     document.querySelector('#nav').classList.remove('is-active');
   })
 
-})
+  // ТАБЫ ======================================================================================================================================================
 
-// ТАБЫ ======================================================================================================================================================
+  //активный таб
+  function slidesPlugin(activeSlide = 0) {
+    const slides = document.querySelectorAll('.working__day-link');
 
-//активный таб
-function slidesPlugin(activeSlide = 0) {
-  const slides = document.querySelectorAll('.working__day-link');
+    slides[activeSlide].classList.add('active');
 
-  slides[activeSlide].classList.add('active');
-
-  for (const slide of slides) {
-    slide.addEventListener('click', () => {
-      clearActiveClasses();
-      slide.classList.add('active');
-    });
-  }
-
-  function clearActiveClasses() {
-    slides.forEach((slide) => {
-      slide.classList.remove('active');
-    });
-  }
-}
-
-slidesPlugin();
-
-document.querySelectorAll('.tabs__btn').forEach(function (tabsBtn) {
-  tabsBtn.addEventListener('click', function (event) {
-    const path = event.currentTarget.dataset.path
-
-    document.querySelectorAll('.tab-content').forEach(function (tabContent) {
-      tabContent.classList.remove('tab-content-active')
-    });
-    document.querySelector(`[data-target="${path}"]`).classList.add('tab-content-active');
-  })
-})
-
-// faq-АККОРДЕОН
-
-$(".js-accordion").accordion({
-  collapsible: true,
-  active: false,
-  icons: false,
-  heightStyle: 'content'
-});
-
-// КНОПКА НАВЕРХ ======================================================================================================================================================
-
-jQuery(document).ready(function () {
-  var btn = $('#button');
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > 300) {
-      btn.addClass('show');
-    } else {
-      btn.removeClass('show');
+    for (const slide of slides) {
+      slide.addEventListener('click', () => {
+        clearActiveClasses();
+        slide.classList.add('active');
+      });
     }
+
+    function clearActiveClasses() {
+      slides.forEach((slide) => {
+        slide.classList.remove('active');
+      });
+    }
+  }
+
+  slidesPlugin();
+
+  document.querySelectorAll('.tabs__btn').forEach(function (tabsBtn) {
+    tabsBtn.addEventListener('click', function (event) {
+      const path = event.currentTarget.dataset.path
+
+      document.querySelectorAll('.tab-content').forEach(function (tabContent) {
+        tabContent.classList.remove('tab-content-active')
+      });
+      document.querySelector(`[data-target="${path}"]`).classList.add('tab-content-active');
+    })
+  })
+
+  // faq-АККОРДЕОН
+
+  $(".js-accordion").accordion({
+    collapsible: true,
+    active: false,
+    icons: false,
+    heightStyle: 'content'
   });
-  btn.on('click', function (e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: 0 }, '300');
-  });
+
+  // КНОПКА НАВЕРХ ======================================================================================================================================================
+
+  jQuery(document).ready(function () {
+    var btn = $('#button');
+    $(window).scroll(function () {
+      if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+      } else {
+        btn.removeClass('show');
+      }
+    });
+    btn.on('click', function (e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: 0 }, '300');
+    });
+
+  })
 });
